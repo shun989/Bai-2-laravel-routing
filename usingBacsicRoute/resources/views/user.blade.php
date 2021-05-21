8034eb0a2241b48bf737f14c53b9c398be3b8ce5
shun989 <minhtuanvt89@gmail.com>
2021-05-21 14:14:23 +0700

    Sử dụng route cơ bản.

---
+++ usingBacsicRoute/resources/views/user.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Route</title>
+</head>
+<body>
+<h2>Xin chào {{$name}}</h2>
+<h3>Wellcome <?= $name ?>!</h3>
+</body>
+</html>
--- usingBacsicRoute/routes/web.php
+++ usingBacsicRoute/routes/web.php
@@ -14,5 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "<h2>This is Home page</h2>";
 });
+
+Route::get('/about', function (){
+   echo "<h2>This is About page</h2>";
+});
+
+Route::get('/contact', function (){
+   echo "<h2>This is Contact page</h2>";
+});
+
+Route::get('/user', function (){
+   return view('user',['name' =>'Minh Tuan']) ;
+});
+
+Route::get('/user/{name}', function ($name){
+   echo "<h2>User name is $name</h2>";
+});
+
+Route::get('/user-name/{name?}', function ($name = 'Admin'){
+    echo "<h2>User name is $name</h2>";
+});
+
+Route::get('/', 'App\Http\Controllers\HomeController@index');
